fbea20c92b7161a63dfbd83938168cf12fc0b8ba
Deniz Bayar <deniz.bayar@prod.hclpnp.com>
2020-08-31 09:32:35 +0200

    use rxJs with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react';
+import {from, BehaviorSubject} from 'rxjs';
+import {filter, mergeMap, debounceTime, distinctUntilChanged} from 'rxjs/operators';
+
+const getPokemonByName = async name=>{
+  const {results:allPokemons} = await fetch('https://pokeapi.co/api/v2/pokemon?offset=300&limit=100').then(result=>result.json());
+  return allPokemons.filter(pokemon=>pokemon.name.includes(name));
+}
+
+let searchSubject = new BehaviorSubject("");
+
+//Bulbasour
+
+let searchResultObservable = searchSubject.pipe(
+  filter(val=>val.length>1),
+  debounceTime(750),
+  distinctUntilChanged(),
+
+  // mergeMap(val=> from(getPokemonByName(val)) )
+)
+
+// 750 ms Bulbosour
+
+searchResultObservable.subscribe()
+
+
+
+const useObservable= (observable, setter)=>{
+  useEffect(() => {
+    let subscription = observable.subscribe((val)=>setter(val));
+
+    return ()=> subscription.unsubscribe();
+  }, [observable,setter])
+}
+
 
 function App() {
+  const [name, setName] = useState('');
+  const [results, setResults] = useState([]);
+  useObservable(searchResultObservable, setResults);
+
+ 
+
+  const onInputChange = (e)=>{
+    const newValue=e.target.value;
+    setName(newValue);
+    searchSubject.next(newValue);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <input type="text" placeholder="Enter a name" onChange={onInputChange}></input>
+  <p>Here is the number: {name}</p>
+
+  {results.map(pokemon=> (<div key={pokemon.name}>
+    {pokemon.name}
+  </div>))}
+  </div>
   );
 }
 
